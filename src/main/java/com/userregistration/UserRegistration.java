702f3cb07052d5aed629abae86faeab4ef26183d
anamfazal <anamfazal94@gmail.com>
2020-10-20 12:40:26 +0530

    Added: Test cases for Password validation

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -9,6 +9,7 @@ public class UserRegistration {
     private String firstName;
     private static final String REGEX_NAME = "^[A-Z]{1}[a-z]{2,}$";
     private static final String REGEX_EMAIL = "^[A-Za-z0-9]+([-\\.\\+\\_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([\\.\\,]{1}[a-z]{2,3}){0,1}$";
+    private static final String REGEX_PASSWORD = "^(?=.*[A-Z](?=.*\\d))(?=.*[\\@\\$\\!\\%\\*\\?\\&])[A-Za-z\\d\\@\\$\\!\\%\\*\\?\\&]{8,}$";
 
     /* method to validate first name */
     public boolean validateFirstName(String firstName) {
@@ -26,5 +27,12 @@ public class UserRegistration {
         Pattern pattern = Pattern.compile(REGEX_EMAIL);
         return pattern.matcher(email).matches();
     }
+
+    /* method to validate password : min 8 characters , min 1 uppercase , min 1 digit , exactly 1 special character */
+    public boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(REGEX_PASSWORD);
+        return pattern.matcher(password).matches();
+    }
+
 
 }
