fbc209511108d8cb32223b96f75f14f2af20a5cb
anamfazal <anamfazal94@gmail.com>
2020-10-20 11:16:57 +0530

    Added: Test cases for email validation

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private String firstName;
     private static final String REGEX_NAME = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String REGEX_EMAIL = "^[A-Za-z0-9]+([-\\.\\+\\_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([\\.\\,]{1}[a-z]{2,3}){0,1}$";
 
     /* method to validate first name */
     public boolean validateFirstName(String firstName) {
@@ -20,6 +21,10 @@ public class UserRegistration {
         Pattern pattern = Pattern.compile(REGEX_NAME);
         return pattern.matcher(lastName).matches();
     }
-
+    /* method to validate email */
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(REGEX_EMAIL);
+        return pattern.matcher(email).matches();
+    }
 
 }
